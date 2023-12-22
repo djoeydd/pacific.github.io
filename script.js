a71588a824ecfbb405d9bb4f19d06bb3a02f45be
Joseph Daniszewski <60105202+djoeydd@users.noreply.github.com>
2023-12-23 02:13:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const map = L.map('map', {
   const markers = L.markerClusterGroup({
     showCoverageOnHover: false,
     maxClusterRadius: 55,
-    disableClusteringAtZoom: 10,
+    disableClusteringAtZoom: 8,
   });
   
   const shipFilters = document.querySelectorAll('.ship-filter');
